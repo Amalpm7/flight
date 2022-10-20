09fcae1e1a33fcb82ac001817a315d678ecdb25d
Amalpm7 <amal.p@nestgroup.net>
2022-10-20 10:37:30 +0530

    first

--- src/main/java/com/example/flightbackend/Controller/FlightController.java
+++ src/main/java/com/example/flightbackend/Controller/FlightController.java
@@ -5,6 +5,7 @@ import com.example.flightbackend.Model.FlightModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @RestController
@@ -24,5 +25,14 @@ public class FlightController {
     @GetMapping("/viewFlight")
     public List<FlightModel> allflight(){
         return (List<FlightModel>) dao.findAll();
+    }
+
+
+    @CrossOrigin(origins = "*")
+    @Transactional
+    @PostMapping(path = "/deleteflight" ,consumes = "application/json", produces = "application/json")
+    public String deleteflight(@RequestBody FlightModel flight){
+        dao.deleteflightbyid(flight.getId());
+        return "{status:'success'}";
     }
 }
--- src/main/java/com/example/flightbackend/Dao/FlightDao.java
+++ src/main/java/com/example/flightbackend/Dao/FlightDao.java
@@ -1,7 +1,12 @@
 package com.example.flightbackend.Dao;
 
 import com.example.flightbackend.Model.FlightModel;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface FlightDao extends CrudRepository<FlightModel,Integer> {
+    @Modifying
+    @Query(value = "DELETE FROM `flights` WHERE `id`= :id",nativeQuery = true)
+    void deleteflightbyid(Integer id);
 }
